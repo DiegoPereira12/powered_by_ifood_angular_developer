952ddb8eb8f8aceb55a189fb4d87d4b39a609de8
unknown <diegoo.junior55@gmail.com>
2023-05-05 08:52:38 -0300

    update files 05052023

---
+++ Primeiras Páginas Interativas com JavaScript/4. Arrays e Estruturas de Repetição em JavaScript/4. 1 - Arrays e Estruturas de Repetição/aula_01_listas.js
@@ -0,0 +1,39 @@
+// Lista
+
+const alunos = ['João', 'Vitor', 'Marina']
+
+console.log(alunos);
+
+// Acessar posição especifica da lista
+
+console.log(alunos[0]);
+
+// Adicionar item na lista
+
+alunos.push('Diego');
+
+console.log(alunos);
+
+//Ou
+
+alunos[2] = 'Livia'
+
+console.log(alunos);
+
+// Remover item da lista(remove o último item da lista)
+
+alunos.pop();
+
+console.log(alunos);
+
+// Remover item da lista(remove o primeiro item da lista)
+
+alunos.shift();
+
+console.log(alunos);
+
+// Mostrar tamanho da lista
+
+console.log(alunos.length)
+
+
---
+++ Primeiras Páginas Interativas com JavaScript/4. Arrays e Estruturas de Repetição em JavaScript/4. 1 - Arrays e Estruturas de Repetição/aula_02_repetição.js
@@ -0,0 +1,25 @@
+/*const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push()
+*/
+
+// Laços de repetição com for(exemplo)
+
+for (let i = 0; i < 10; i++) {    console.log(i)
+
+}
+
+// Laços de repetição com for(exemplo)
+
+const nome = 'Diego Pereira';
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i])
+
+}
+
