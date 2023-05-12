eccfb1a0d6ade9973e6f4afc97b7fbb87d26169a
unknown <diegoo.junior55@gmail.com>
2023-05-11 23:09:27 -0300

    update files 11052023

---
+++ Primeiras Páginas Interativas com JavaScript/5. Importação e Exportação com JavaScript/5. 2  - Praticando os Conceitos de Importação e Exportação/main.js
@@ -0,0 +1,22 @@
+/*
+    Uma sala contem 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+    Faça um programa que receba os 5 número sorteados para os alunos e mostre o maior número sorteando.
+
+    Dados de entrada:
+    5
+    50
+    10
+    98
+    23
+
+    Saída
+    98
+*/
+
+const { gets, print } =  require('./funcoes-auxiliares.js');
+
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
