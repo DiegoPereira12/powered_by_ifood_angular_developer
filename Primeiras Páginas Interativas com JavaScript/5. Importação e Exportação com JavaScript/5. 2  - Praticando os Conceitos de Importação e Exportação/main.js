25bdfb2acb09421312cefab153972bf043078c4c
unknown <diegoo.junior55@gmail.com>
2023-05-12 21:11:29 -0300

    update files 12052023

--- Primeiras Páginas Interativas com JavaScript/5. Importação e Exportação com JavaScript/5. 2  - Praticando os Conceitos de Importação e Exportação/main.js
+++ Primeiras Páginas Interativas com JavaScript/5. Importação e Exportação com JavaScript/5. 2  - Praticando os Conceitos de Importação e Exportação/main.js
@@ -13,10 +13,23 @@
     98
 */
 
-const { gets, print } =  require('./funcoes-auxiliares.js');
+const { gets, print } = require('./funcoes-auxiliares.js');
 
-print(gets());
-print(gets());
-print(gets());
-print(gets());
-print(gets());
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numerosSorteado = gets();
+    numerosSorteados.push(numerosSorteado)
+
+}
+
+let maiorValor = 0;
+
+for(let i = 0; i < numerosSorteados.length; i++){
+    const numerosSorteado = numerosSorteados[i];
+    if (numerosSorteado > maiorValor){
+        maiorValor = numerosSorteado;
+    }
+}
+
+print(maiorValor)
