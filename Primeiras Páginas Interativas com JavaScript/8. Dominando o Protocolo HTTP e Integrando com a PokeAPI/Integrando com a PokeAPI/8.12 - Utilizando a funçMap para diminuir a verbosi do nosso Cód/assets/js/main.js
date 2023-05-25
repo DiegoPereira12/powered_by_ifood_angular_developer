ad05328ad90137a593124601b1df99635cbe13de
unknown <diegoo.junior55@gmail.com>
2023-05-25 09:05:07 -0300

    update files 25052023

--- Primeiras Páginas Interativas com JavaScript/8. Dominando o Protocolo HTTP e Integrando com a PokeAPI/Integrando com a PokeAPI/8.12 - Utilizando a funçMap para diminuir a verbosi do nosso Cód/assets/js/main.js
+++ Primeiras Páginas Interativas com JavaScript/8. Dominando o Protocolo HTTP e Integrando com a PokeAPI/Integrando com a PokeAPI/8.12 - Utilizando a funçMap para diminuir a verbosi do nosso Cód/assets/js/main.js
@@ -1,12 +1,9 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
             <span class="number">#001</span>
-            <span class="name">$(pokemon.name)</span>
+            <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
@@ -15,7 +12,7 @@ function convertPokemonToLi(pokemon) {
                 </ol>
 
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-                    alt="$(pokemon.name)">
+                    alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -24,15 +21,8 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
-    const lisItems = []
-
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        lisItems.push(convertPokemonToLi(pokemon))
-      
-    }
-
-    console.log(lisItems)
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
 })
+
 
